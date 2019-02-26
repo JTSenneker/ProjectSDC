ac04fec67b9bcf36c583be751b5ea6e843300a67
nategravelle <n8thegr8.nrg@gmail.com>
2019-02-26 10:29:58 -0500

    imported project

---
+++ project sdc - movement/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 movement;
+    public float rotate;
+    public float walkSpeed;
+    public float runSpeed;
+    public float crouchspeed;
+    public float energydepletion;
+    private float currentSpeed;
+    private Rigidbody rb;
+    private Quaternion Quat;
+    private bool crouch = false;
+    public bool running;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = walkSpeed;
+        GetComponent<Stamina>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (crouch == false)
+            {
+                crouch = true;
+            }
+            else
+            {
+                crouch = false;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey("a")||Input.GetKey("s")||Input.GetKey("d")||Input.GetKey("w"))
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            move(h, v);
+            currentSpeed = walkSpeed;
+        }
+        running = false;
+    }
+    void move(float h,float v)
+    {
+        if(crouch)
+        {
+            currentSpeed = crouchspeed;
+        }
+        if (Input.GetKey(KeyCode.LeftShift)&&Stamina.energy > Stamina.lowestenergy && crouch == false)
+        {
+            running = true;
+            currentSpeed = runSpeed;
+            Stamina.energy -= energydepletion*Time.deltaTime;
+            if(Stamina.energy<30)
+            {
+                Stamina.energy = 30;
+            }
+        }
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * currentSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+        transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(movement),rotate);
+        transform.Translate(movement * currentSpeed * Time.deltaTime, Space.World);
+    }
+}
---
+++ project sdc - movement/Assets/scripts/UpdateText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour
+{
+    Text text;
+    string newtext;
+    void Start()
+    {
+        GetComponent<Stamina>();
+        text = GetComponent<Text>();
+    }
+    void Update()
+    {
+        text.text = Stamina.energy.ToString();
+    }
+}
