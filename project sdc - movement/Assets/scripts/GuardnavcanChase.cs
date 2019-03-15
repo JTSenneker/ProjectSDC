5c46f4955b296beada670e29b7a58873729714dd
Seaserpent31 <47004350+Seaserpent31@users.noreply.github.com>
2019-03-15 11:25:47 -0400

    Guard Patrols And Chases
    Guard patrols waypoints and chases the player
    
    TODO:
    Create new enemy prefab

---
+++ project sdc - movement/Assets/scripts/GuardnavcanChase.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardnavcanChase : MonoBehaviour {
+    UnityEngine.AI.NavMeshAgent nav;
+    public Transform pathHolder;
+    public float speed;
+    public float runspeed;
+    public float waitTime;
+    public float turnSpeed;
+    public float fieldOfViewAngle;
+    public float sightdistance;
+    public Vector3 movement;
+    public Transform[] targetWaypoints;
+    float timer;
+
+    private Collider B_Collider;
+    private Collider C_Collider;
+    private static bool ischasing;
+    
+    private Transform player;
+    private Transform up;
+    private int nextpoint=1;
+    private int totalPoints=0;
+    float viewAngle;
+    void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform waypoint in pathHolder)
+        {
+           
+            Gizmos.DrawSphere(waypoint.position, .3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+       
+    }
+
+
+
+    void Update()
+    {
+     
+        if (ischasing == true)
+        {
+            nav.SetDestination(player.position);
+            gameObject.tag = ("ChasingGuard");
+        }
+        if (ischasing == false)
+        {
+            NextPoint();
+            gameObject.tag = ("Guard");
+        }
+    }
+
+    void Awake()
+    {
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+
+        ischasing = false;
+        
+        B_Collider = GetComponent<BoxCollider>();
+        C_Collider = GetComponent<CapsuleCollider>();
+    }
+
+
+    void NextPoint()
+    {
+        nav.SetDestination(targetWaypoints[nextpoint].position);
+        if (Vector3.Distance(transform.position, targetWaypoints[nextpoint].position)<=.05f)
+        {
+            timer += Time.deltaTime;
+            Vector3 dirTurnToLookTarget = (targetWaypoints[(nextpoint + 1) % targetWaypoints.Length].position - transform.position).normalized;
+            float targetAngle = 90 - Mathf.Atan2(dirTurnToLookTarget.z, dirTurnToLookTarget.x) * Mathf.Rad2Deg;
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            if (timer>= waitTime)
+            {
+                timer = 0;
+                nextpoint = (nextpoint + 1) % targetWaypoints.Length;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        detect(other);
+       
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            ischasing = false;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        detect(other);
+    }
+
+    void detect(Collider other)
+    {
+        if (other.gameObject.tag == ("Player") || other.gameObject.tag == ("ChasingGuard"))
+        {
+
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+            if (angle <= fieldOfViewAngle * .5f)
+            {
+
+                RaycastHit hit;
+                C_Collider.enabled = false;
+                B_Collider.enabled = false;
+                if (Physics.Raycast(transform.position, direction.normalized, out hit, sightdistance))
+                {
+
+                    if (hit.collider.gameObject.tag == ("Player") || hit.collider.gameObject.tag == ("ChasingGuard"))
+                    {
+                        Debug.Log("sight");
+                        ischasing = true;
+                        C_Collider.enabled = true;
+                        B_Collider.enabled = true;
+                    }
+                    else
+                    {
+                        ischasing = false;
+                        C_Collider.enabled = true;
+                        B_Collider.enabled = true;
+                    }
+                }
+                else
+                {
+                    ischasing = false;
+                    C_Collider.enabled = true;
+                    B_Collider.enabled = true;
+                }
+                C_Collider.enabled = true;
+                B_Collider.enabled = true;
+            }
+            else
+            {
+                ischasing = false;
+            }
+           
+        }
+        else
+        {
+            ischasing = false;
+        }
+    }
+    
+
+
+
+
+}
+
+
---
+++ project sdc - movement/Assets/scripts/player.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    public float speed;
+    private Rigidbody Rb;
+    private int count;
+    public float pickupRange;
+    public bool inPickUpRange = false;
+    public float fieldOfViewAngle;
+    public float sightdistance;
+
+    private Collider B_Collider;
+    private Collider C_Collider;
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start() {
+        Rb = GetComponent<Rigidbody>();
+        B_Collider = GetComponent<BoxCollider>();
+        C_Collider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Rb.AddForce(movement * speed);
+       
+
+
+    }   
+
+
+
+
+
+
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup" && Input.GetKeyDown("e"))
+        {
+           
+           
+            detect(other);
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup"&& Input.GetKeyDown("e"))
+        {
+            inPickUpRange = true;
+            
+        }
+
+    }
+    void OnTriggerleave(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            inPickUpRange = false;
+           
+        }
+
+    }
+
+    void detect(Collider other)
+    {
+        if (other.gameObject.tag == ("Pickup"))
+        {
+            Debug.Log("pickup");
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+            if (angle <= fieldOfViewAngle * .5f)
+            {
+                
+                RaycastHit hit;
+                C_Collider.enabled = false;
+                B_Collider.enabled = false;
+                if (Physics.Raycast(transform.position, direction.normalized, out hit, sightdistance))
+                {
+
+                    if (hit.collider.gameObject.tag == ("Pickup"))
+                    {
+                        Debug.Log("sight");
+                        Destroy(other.gameObject);
+                        C_Collider.enabled = true;
+                        B_Collider.enabled = true;
+                    }
+                    else
+                    {
+
+                        C_Collider.enabled = true;
+                        B_Collider.enabled = true;
+                    }
+                }
+                else
+                {
+
+                    C_Collider.enabled = true;
+                    B_Collider.enabled = true;
+                }
+                C_Collider.enabled = true;
+                B_Collider.enabled = true;
+            }
+        }
+    }
+}
