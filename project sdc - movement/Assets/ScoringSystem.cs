50875cce7309ad7731ee13fbb6dbbdc1d5915b2b
PolyEthaniel <PolyEthaniel@users.noreply.github.com>
2019-05-10 11:22:21 -0400

    Implemented Win State Item

---
+++ project sdc - movement/Assets/ScoringSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public int ExitLevel;
+    public string Nextlevel;
+
+   void OnTriggerEnter(Collider other)
+    {
+       ExitLevel = ExitLevel += 1;
+        Debug.Log(ExitLevel);
+        Destroy(gameObject);
+        GoToNextLevel();
+    }
+
+
+
+
+    public void GoToNextLevel()
+    {
+        if (ExitLevel >= 1)
+        {
+            SceneManager.LoadScene(Nextlevel);
+        }
+    }
+}
